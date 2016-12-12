1c84e5e8a4920c8624888577ab72161cef0d7742
kenk42292 <kenk42292@gmail.com>
2016-12-12 06:38:58 -0500

    softmax layer feedforward, vanillafeedforward layer feedforward

--- src/layer/Softmax.cpp
+++ src/layer/Softmax.cpp
@@ -16,9 +16,17 @@ Softmax::~Softmax() {
 	// TODO Auto-generated destructor stub
 }
 
+arma::Cube<double> Softmax::softmax(const arma::Cube<double>& z) {
+	arma::Col<double> exp_z = arma::exp(z);
+	return exp_z / arma::accu(exp_z);
+}
 
 arma::field<arma::Cube<double>> Softmax::feedForward(const arma::field<arma::Cube<double>>& zs) {
-
+	arma::field<arma::Cube<double>> ys(zs.size());
+	for (unsigned int i=0; i<zs.size(); ++i) {
+		ys[i] = softmax(zs[i]);
+	}
+	return ys;
 }
 
 arma::field<arma::Cube<double>> Softmax::backProp(const arma::field<arma::Cube<double>>& deltas) {
